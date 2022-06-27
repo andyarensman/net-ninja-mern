c95c8a845db49efe9ac2803ec237df95458fa78b
andyarensman <86622025+andyarensman@users.noreply.github.com>
2022-06-27 10:44:01 -0700

    lesson 10 - new workout form

--- frontend/src/components/WorkoutDetails.js
+++ frontend/src/components/WorkoutDetails.js
@@ -3,7 +3,7 @@ const WorkoutDetails = ({ workout }) => {
     <div className="workout-details">
       <h4>{workout.title}</h4>
       <p><strong>Load (kg): </strong>{workout.load}</p>
-      <p><strong>Reps (kg): </strong>{workout.reps}</p>
+      <p><strong>Reps: </strong>{workout.reps}</p>
       <p>{workout.createdAt}</p>
     </div>
   )
