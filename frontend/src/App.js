fc629b7b3ee2ad1e251a53083a2368eb0f117d3b
andyarensman <86622025+andyarensman@users.noreply.github.com>
2022-07-28 10:36:35 -0700

    auth lesson 9 - login and signup forms

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,10 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Navbar from './components/Navbar'
 
 //pages & components
 import Home from './pages/Home'
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Navbar from './components/Navbar'
 
 function App() {
   return (
@@ -14,6 +16,14 @@ function App() {
             <Route 
               path="/"
               element={<Home />}
+            />
+            <Route 
+              path="/login"
+              element={<Login />}
+            />
+            <Route 
+              path="/signup"
+              element={<Signup />}
             />
           </Routes>
         </div>
